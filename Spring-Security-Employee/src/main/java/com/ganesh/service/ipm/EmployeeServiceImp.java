a121c16a874208472783edde93fc19b3c49f0d2d
Ganesh <ganeshgarje716@gmail.com>
2026-07-03 15:07:47 +0530

    @Service Annotation

--- Spring-Security-Employee/src/main/java/com/ganesh/service/ipm/EmployeeServiceImp.java
+++ Spring-Security-Employee/src/main/java/com/ganesh/service/ipm/EmployeeServiceImp.java
@@ -1,7 +1,10 @@
 package com.ganesh.service.ipm;
 
+import org.springframework.stereotype.Service;
+
 import com.ganesh.service.EmployeeService;
 
+@Service
 public class EmployeeServiceImp implements EmployeeService{
 
 }

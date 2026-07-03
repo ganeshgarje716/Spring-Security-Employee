6681f7bc445b3d3862b75cd034bb6d82c030eb53
Ganesh <ganeshgarje716@gmail.com>
2026-07-03 15:08:24 +0530

    Extends JpaRepository Interface

--- Spring-Security-Employee/src/main/java/com/ganesh/repository/EmployeeRepository.java
+++ Spring-Security-Employee/src/main/java/com/ganesh/repository/EmployeeRepository.java
@@ -1,5 +1,9 @@
 package com.ganesh.repository;
 
-public interface EmployeeRepository {
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.ganesh.entity.Employee;
+
+public interface EmployeeRepository extends JpaRepository<Employee, Integer>{
 
 }

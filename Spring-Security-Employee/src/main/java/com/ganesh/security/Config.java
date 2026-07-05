debc95beae66569339f497d0653efc85662fe6b4
Ganesh <ganeshgarje716@gmail.com>
2026-07-06 00:07:11 +0530

    Which User Can be handling the API Logic

--- Spring-Security-Employee/src/main/java/com/ganesh/security/Config.java
+++ Spring-Security-Employee/src/main/java/com/ganesh/security/Config.java
@@ -1,8 +1,59 @@
 package com.ganesh.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+
+import com.ganesh.service.CustomUserDetailsService;
 
 @Configuration
+@EnableWebSecurity
 public class Config {
+	
+	
+	@Autowired
+	CustomUserDetailsService customUserDetailsService;
+	
+	
+	@Bean
+	public DaoAuthenticationProvider authenticateUser() {
+		
+		DaoAuthenticationProvider provider = new DaoAuthenticationProvider(customUserDetailsService);
+		
+		provider.setPasswordEncoder(encoder());
+		
+		return provider;
+	}
+	
+	
+	@Bean
+	public BCryptPasswordEncoder encoder() {
+		
+		return new BCryptPasswordEncoder();
+	}
+	
+	
+	@Bean
+	public SecurityFilterChain filterChain(HttpSecurity http) {
+		
+		http.csrf(csrf->csrf.disable());
+		
+		http.httpBasic(Customizer.withDefaults());
+		
+		http.authorizeHttpRequests(auth->{
+			
+			auth.requestMatchers("/api/v1/employee/save").hasRole("ADMIN");
+			
+			auth.requestMatchers("/auth/create-user").permitAll();
+		});
+		
+		return http.build();
+	}
 
 }

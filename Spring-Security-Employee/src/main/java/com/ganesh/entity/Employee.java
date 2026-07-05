ba961cd2fbacfe7d2866516833d0197002c92056
Ganesh <ganeshgarje716@gmail.com>
2026-07-05 14:28:53 +0530

    Add Variables

--- Spring-Security-Employee/src/main/java/com/ganesh/entity/Employee.java
+++ Spring-Security-Employee/src/main/java/com/ganesh/entity/Employee.java
@@ -1,6 +1,14 @@
 package com.ganesh.entity;
 
+import java.time.LocalDate;
+
+import org.hibernate.annotations.CreationTimestamp;
+
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -11,6 +19,49 @@ import lombok.NoArgsConstructor;
 @Entity
 public class Employee {
 	
-
-
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	@Column(unique = true)
+	private String email;
+	
+	@Column(unique = true)
+	private String mobileNo;
+	
+	private String gender;
+	
+	private String dep;
+	
+	private Double salary;
+	
+	@CreationTimestamp
+	private LocalDate joningDate;
+	
+	private LocalDate dob;
+	
+	private String status;
+	
+	private Boolean isMarried;
+	
+	private Integer experience;
+	
+	private String educationQualification;
+	
+	private String address;
+	
+	private Integer age;
+	
+	private String designation;
+	
+	
+	
+	
+	
+	
 }

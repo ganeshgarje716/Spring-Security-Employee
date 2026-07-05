96f72325efb6c961b25e2a84a1f70d249d9e574c
Ganesh <ganeshgarje716@gmail.com>
2026-07-06 00:05:58 +0530

    User API Handling logic

--- Spring-Security-Employee/src/main/java/com/ganesh/service/CustomUserDetailsService.java
+++ Spring-Security-Employee/src/main/java/com/ganesh/service/CustomUserDetailsService.java
@@ -1,15 +1,43 @@
 package com.ganesh.service;
 
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
 
+import com.ganesh.entity.User;
+import com.ganesh.repository.UserRepository;
+
+@Service
 public class CustomUserDetailsService implements UserDetailsService{
 
+	
+	
+	@Autowired
+	UserRepository userRepository;
+	
+	
+	
 	@Override
 	public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
-		// TODO Auto-generated method stub
-		return null;
+
+		Optional<User> byUserName = userRepository.findByUsername(username);
+		
+		if (byUserName.isPresent()) {
+			
+			User user = byUserName.get();
+			
+			return org.springframework.security.core.userdetails.User
+					.withUsername(user.getUsername())
+					.password(user.getPassword())
+					.roles(user.getRole())
+					.build();
+		}
+		
+		throw new UsernameNotFoundException("User Not Found With UserName = "+username);
 	}
 
 }

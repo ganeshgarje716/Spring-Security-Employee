dc1f67b95b8126f5f2383f8aa74e4638f71c19e6
Ganesh <ganeshgarje716@gmail.com>
2026-07-05 16:11:49 +0530

    Create User Class in entity

---
+++ Spring-Security-Employee/src/main/java/com/ganesh/entity/User.java
@@ -0,0 +1,26 @@
+package com.ganesh.entity;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+
+@Entity
+@Data
+public class User {
+
+	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Integer id;
+	
+	@Column(unique = true)
+	private String username;
+	
+	private String password;
+	
+	private String role;
+	
+}

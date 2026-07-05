acf140cc4a7756b597d0eecc9507c88b1be1c1e0
Ganesh <ganeshgarje716@gmail.com>
2026-07-05 14:32:35 +0530

    Add Variables in Dto

--- Spring-Security-Employee/src/main/java/com/ganesh/Dto/EmployeeDto.java
+++ Spring-Security-Employee/src/main/java/com/ganesh/Dto/EmployeeDto.java
@@ -1,8 +1,42 @@
 package com.ganesh.Dto;
 
+import java.time.LocalDate;
+
 import lombok.Data;
 
 @Data
 public class EmployeeDto {
+	
+	
+	
+	private String firstName;
+	
+	private String lastName;
+	
+	private String email;
+	
+	private String mobileNo;
+	
+	private String gender;
+	
+	private String dep;
+	
+	private Double salary;
+	
+	private LocalDate dob;
+	
+	private String status;
+	
+	private Boolean isMarried;
+	
+	private Integer experience;
+	
+	private String educationQualification;
+	
+	private String address;
+	
+	private Integer age;
+	
+	private String designation;
 
 }

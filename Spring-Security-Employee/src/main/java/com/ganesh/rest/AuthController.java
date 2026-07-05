47a1cec4ed2290bf9cd515193278de472d756ed3
Ganesh <ganeshgarje716@gmail.com>
2026-07-06 00:04:52 +0530

    Create saveUser() Method

--- Spring-Security-Employee/src/main/java/com/ganesh/rest/AuthController.java
+++ Spring-Security-Employee/src/main/java/com/ganesh/rest/AuthController.java
@@ -1,6 +1,8 @@
 package com.ganesh.rest;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -8,6 +10,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.ganesh.entity.User;
+import com.ganesh.repository.UserRepository;
 
 @RestController
 @RequestMapping("/auth")
@@ -15,13 +18,31 @@ public class AuthController {
 	
 	
 	@Autowired
+	UserRepository userRepository;
+	
+	@Autowired
 	BCryptPasswordEncoder bCryptPasswordEncoder;
 
 	
 	
 	@PostMapping("/create-user")
-	public void createUsers(@RequestBody User user) {
+	public ResponseEntity<User> createUsers(@RequestBody User user) {
 		
+		user.setPassword(bCryptPasswordEncoder.encode(user.getPassword()));
+		
+		if (user.getUsername().equals("Garje")) {
+			
+			user.setRole("ADMIN");
+		}
+		else {
+			
+			user.setRole("USER");
+		}
+		
+		
+		User saved = userRepository.save(user);
+		
+		return new ResponseEntity<User>(saved, HttpStatus.CREATED);
 	}
 	
 	

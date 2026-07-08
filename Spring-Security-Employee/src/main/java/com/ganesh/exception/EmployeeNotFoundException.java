f586ca4e737299a11daef9e6dc48482d4a33aa16
Ganesh <ganeshgarje716@gmail.com>
2026-07-08 18:00:48 +0530

    Create EmployeeNotFoundException Class

---
+++ Spring-Security-Employee/src/main/java/com/ganesh/exception/EmployeeNotFoundException.java
@@ -0,0 +1,12 @@
+package com.ganesh.exception;
+
+public class EmployeeNotFoundException extends RuntimeException {
+	
+	
+	public EmployeeNotFoundException(String msg) {
+		
+		super(msg);
+		// TODO Auto-generated constructor stub
+	}
+
+}
